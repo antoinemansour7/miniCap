71aa560c3370c463add4a9a40ed475c5a783f07f
antoinemansour7 <129776972+antoinemansour7@users.noreply.github.com>
2025-01-22 13:03:08 -0500

    codecov

--- FindMyClass/jest.setup.js
+++ FindMyClass/jest.setup.js
@@ -1,11 +1,23 @@
 import '@testing-library/jest-native/extend-expect';
+import 'expect';
 
-// Mock expo-font
-jest.mock('expo-font');
-
-// Mock react-native reanimated
-jest.mock('react-native-reanimated', () => {
-  const Reanimated = require('react-native-reanimated/mock');
-  Reanimated.default.call = () => {};
-  return Reanimated;
-});
+// Simplified reanimated mock
+jest.mock('react-native-reanimated', () => ({
+  default: {
+    createAnimatedComponent: jest.fn(),
+    Value: jest.fn(),
+    event: jest.fn(),
+    add: jest.fn(),
+    eq: jest.fn(),
+    set: jest.fn(),
+    cond: jest.fn(),
+    interpolate: jest.fn(),
+    View: jest.fn(),
+    Extrapolate: { CLAMP: jest.fn() },
+    Transition: {
+      Together: 'Together',
+      Out: 'Out',
+      In: 'In',
+    },
+  },
+}));
---
+++ FindMyClass/test/jest-setup.ts
@@ -0,0 +1,4 @@
+import '@testing-library/jest-native/extend-expect';
+import { expect } from '@jest/globals';
+
+global.expect = expect;
