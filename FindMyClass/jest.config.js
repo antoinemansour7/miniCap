daf00349cd02683b2ee63cff5d2315866312d642
antoinemansour7 <129776972+antoinemansour7@users.noreply.github.com>
2025-01-23 14:31:04 -0500

    Everythng working

--- FindMyClass/jest.config.js
+++ FindMyClass/jest.config.js
@@ -1,11 +1,11 @@
+// Updated jest.config.js
 module.exports = {
   preset: 'jest-expo',
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)'
   ],
   setupFiles: ['<rootDir>/jest.setup.js'],
-  testEnvironment: 'jsdom',
   moduleNameMapper: {
-    '^react-native$': 'react-native-web'
-  }
+    '^react-native$': 'react-native',
+  },
 };
