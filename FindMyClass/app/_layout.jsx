7e44df25a850c5335d2c2f8f3a4b204853a66cb5
Nabih21 <126896277+Nabih21@users.noreply.github.com>
2025-01-25 22:08:21 -0500

    Drawer moved to the RIght
    Moved the position of the Drawer to the Right side. Used navigation hook in order to accomplish that

--- FindMyClass/app/_layout.jsx
+++ FindMyClass/app/_layout.jsx
@@ -3,6 +3,11 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { useCallback } from 'react';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
+import { Button, TouchableOpacity } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+
+import { useNavigation  } from '@react-navigation/native';
+import { DrawerActions } from '@react-navigation/native';
 
 export default function Layout() {
   const [fontsLoaded, fontError] = useFonts({
@@ -19,6 +24,7 @@ export default function Layout() {
     return null;
   }
 
+  const navigation = useNavigation(); // Using the navigation hook to move the drawer '3 lines" icon to the right
   return (
     <GestureHandlerRootView style={{ flex: 1 }} onLayout={onLayoutRootView}>
       <Drawer 
@@ -26,25 +32,35 @@ export default function Layout() {
           headerShown: true,
           drawerStyle: {
             backgroundColor: '#fff',
-          }
+          }, 
+          drawerPosition:'right', // To move the drawer to the right
+          headerLeft: () => null, // Removes default 3 lines icon from the left side
+          headerRight: () => ( // Adds 3 lines icon to the right side
+            <TouchableOpacity 
+            onPress={ () => navigation.dispatch(DrawerActions.openDrawer())}
+            style = {{marginRight: 10 }}
+            > 
+            <MaterialIcons  name="menu" size={30} color="#912338" />
+            </TouchableOpacity>
+          ),
         }}
       >
         <Drawer.Screen
-          name="index"
+          name="screens/index"
           options={{
             drawerLabel: 'Home',
             title: 'Home',
           }}
         />
         <Drawer.Screen
-          name="map"
+          name="screens/map"
           options={{
             drawerLabel: 'Map',
             title: 'Map',
           }}
         />
         <Drawer.Screen
-          name="login"
+          name="screens/login"
           options={{
             drawerLabel: 'Login',
             title: 'Login',
@@ -52,7 +68,7 @@ export default function Layout() {
         />
 
         <Drawer.Screen 
-        name = "profile"
+        name = "screens/profile"
         options={{
           drawerLabel: 'Profile',
           title: 'Profile',
