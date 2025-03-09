b32a2fb57ad7609883db6ffa896b89b536bda12b
Nabih21 <126896277+Nabih21@users.noreply.github.com>
2025-03-09 13:45:17 -0400

    Added UxCam
    Added the configuration for the UxCam. It is working

--- FindMyClass/app/index.jsx
+++ FindMyClass/app/index.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { View, StyleSheet } from 'react-native';
 import { useRoute } from '@react-navigation/native'; 
 import ToggleCampusMap from '../components/ToggleCampusMap';
 import FloatingChatButton from '../components/FloatingChatButton';
 import 'react-native-get-random-values'
+import RNUxcam from 'react-native-ux-cam';
+import { uxCamKey } from './secrets';
 
-export default function MapScreen() {  // ✅ Renamed to avoid conflict
+export default function MapScreen() {  // Renamed to avoid conflict
   const route = useRoute();
   const searchText = route?.params?.searchText || ''; // Cleaner fallback
+  
+  useEffect(() => {
+  
+  RNUxcam.optIntoSchematicRecordings(); // Enable iOS screen recordings
+
+  const configuration = {
+      userAppKey: uxCamKey,  // Replace with your UXCam API key
+      enableAutomaticScreenNameTagging: false,
+      enableAdvancedGestureRecognition: true, // Default is true
+      enableImprovedScreenCapture: true, // Improves Android screen capture
+      occlusions: [], // Add occlusion settings if needed
+    };
+
+    RNUxcam.startWithConfiguration(configuration);
+  }, []);
+
+
 
   return (
     <View style={styles.container} testID="map-container">
