b703bd29040a74f846bcf4537f0f36b45cc6e265
Nabih21 <126896277+Nabih21@users.noreply.github.com>
2025-03-09 22:55:47 -0400

    Moved UXCam
    Moved the UXCam component to the _layout.jsx in order to let the the UXCam access the different screen names

--- FindMyClass/app/index.jsx
+++ FindMyClass/app/index.jsx
@@ -4,27 +4,12 @@ import { useRoute } from '@react-navigation/native';
 import ToggleCampusMap from '../components/ToggleCampusMap';
 import FloatingChatButton from '../components/FloatingChatButton';
 import 'react-native-get-random-values'
-import RNUxcam from 'react-native-ux-cam';
-import { uxCamKey } from './secrets';
+
 
 export default function MapScreen() {  // Renamed to avoid conflict
   const route = useRoute();
   const searchText = route?.params?.searchText || ''; // Cleaner fallback
   
-  useEffect(() => {
-  
-  RNUxcam.optIntoSchematicRecordings(); // Enable iOS screen recordings
-
-  const configuration = {
-      userAppKey: uxCamKey,  // Replace with your UXCam API key
-      enableAutomaticScreenNameTagging: false,
-      enableAdvancedGestureRecognition: true, // Default is true
-      enableImprovedScreenCapture: true, // Improves Android screen capture
-      occlusions: [], // Add occlusion settings if needed
-    };
-
-    RNUxcam.startWithConfiguration(configuration);
-  }, []);
 
 
 
