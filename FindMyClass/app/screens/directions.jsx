1becd7f9e602217b3ce00a2f25d3c9ddc9cb04a3
Nabih21 <126896277+Nabih21@users.noreply.github.com>
2025-03-24 00:38:18 -0400

    Update directions.jsx
    removed unwanted comment

--- FindMyClass/app/screens/directions.jsx
+++ FindMyClass/app/screens/directions.jsx
@@ -131,7 +131,6 @@ const path = finder.findPath(
        } else {
            try {
                parsedDestination = JSON.parse(params.destination);
-               console.log("Parsed destination:", parsedDestination);
            } catch (error) {
                console.error("Error parsing destination:", error);
                errorMessage = "Error: Invalid destination format.";
