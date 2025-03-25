a912c15f36205dfaf15cb96649d65e892755e96f
AdamYoshi <adamchami012@gmail.com>
2025-03-25 17:16:06 -0400

    Added VL-2 Coordinates

--- FindMyClass/components/rooms/VanierBuildingRooms.js
+++ FindMyClass/components/rooms/VanierBuildingRooms.js
@@ -396,6 +396,166 @@ const vanierLibraryFloors = {
 
   },
 
+  // VL-2
+  2: {
+    startLocation: {
+      id: "VL2-start",
+      name: "2nd Floor Entrance",
+      location: { x: 9, y: 4 },
+      type: "start"
+    },
+    rooms: [
+        {
+            id: "VL-202",
+            name: "VL-202",
+            location: { x: 3, y: 1 },
+        }, 
+        {
+            id: "VL-202.30",
+            name: "VL-202.30",
+            location: { x: 11, y: 4 },
+        },
+        {
+            id: "VL-230",
+            name: "VL-230",
+            location: { x: 16, y: 4 },
+        }, 
+        {
+            id: "VL-228",
+            name: "VL-228",
+            location: { x: 14, y: 5 },
+        }, 
+        {
+            id: "VL-229",
+            name: "VL-229",
+            location: { x: 17, y: 5 },
+        }, 
+        {
+            id: "VL-227",
+            name: "VL-227",
+            location: { x: 17, y: 7 },
+        }, 
+        {
+            id: "VL-225",
+            name: "VL-225",
+            location: { x: 17, y: 8 },
+        }, 
+        {
+            id: "VL-203-1",
+            name: "VL-201-1",
+            location: { x: 10, y: 11 },
+        },
+        {
+            id: "VL-203-2",
+            name: "VL-203-2",
+            location: { x: 11, y: 11 },
+        },
+        {
+            id: "VL-223",
+            name: "VL-223",
+            location: { x: 17, y: 11 },
+        },
+        {
+            id: "VL-201",
+            name: "VL-201",
+            location: { x: 6, y: 12 },
+        },
+        {
+            id: "VL-203",
+            name: "VL-203",
+            location: { x: 11, y: 12 },
+        },
+        {
+            id: "VL-204",
+            name: "VL-204",
+            location: { x: 11, y: 13 },
+        },
+        {
+            id: "VL-221",
+            name: "VL-221",
+            location: { x: 17, y: 13 },
+        },
+        {
+            id: "VL-240",
+            name: "VL-240",
+            location: { x: 5, y: 14 },
+        },
+        {
+            id: "VL-297",
+            name: "VL-297",
+            location: { x: 7, y: 14 },
+        },
+        {
+            id: "VL-205",
+            name: "VL-205",
+            location: { x: 13, y: 14 },
+        },
+    ],
+    bathrooms: [
+        {
+            id: "VE-216",
+            name: "bathroom-female-1",
+            location: { x: 13, y: 13 },
+        },  
+        {
+            id: "VE-210",
+            name: "bathroom-female-2",
+            location: { x: 12, y: 13 },
+        },   
+        {
+            id: "VE-218",
+            name: "bathroom-male-1",
+            location: { x: 13, y: 15 },
+        },  
+        {
+            id: "VE-212",
+            name: "bathroom-male-2",
+            location: { x: 12, y: 15 },
+        },    
+     ],
+    stairs: [
+        {
+            id: "VL-2-stairs-1",
+            name: "Stairs 1",
+            location: { x: 1, y: 1 },
+        },
+        {
+            id: "VL-2-stairs-2",
+            name: "Stairs 2",
+            location: { x: 4, y: 13 },
+        },
+        {
+            id: "VL-2-stairs-3",
+            name: "Stairs 3",
+            location: { x: 10, y: 3 },
+        },
+        {
+            id: "VL-2-stairs-4",
+            name: "Stairs 4",
+            location: { x: 13, y: 10 },
+        },
+        {
+            id: "VL-2-stairs-5",
+            name: "Stairs 5",
+            location: { x: 17, y: 14 },
+        },
+    ],
+    elevators: [
+        {
+            id: "VL-2-elevator-1",
+            name: "Elevator 1",
+            location: { x: 9, y: 3 },
+        }, 
+    ],
+    exits: [
+        {
+            id: "VL-2-stairs-to-VL-1",
+            name: "Stairs to VL 1",
+            location: { x: 14, y: 10 },
+        },
+    ]
+
+  }
 };
 
 // Helper function to get rooms by floor
