1f4f6a054e047723d371a8c10775df8bb2625216
AdamYoshi <adamchami012@gmail.com>
2025-03-25 17:37:23 -0400

    Finished Vanier floorplans, closes #184

--- FindMyClass/components/rooms/VanierBuildingRooms.js
+++ FindMyClass/components/rooms/VanierBuildingRooms.js
@@ -22,135 +22,149 @@ const vanierLibraryFloors = {
     rooms: [
 
         {
-            id: "VL1-103-1",
-            name: "VL1-103-1",
-            location: { x: 5, y: 13 },
+            id: "VL-101-6",
+            name: "VL-101-6",
+            location: { x: 3, y: 1 },
         },
-
+        
         {
-            id: "VL1-197",
-            name: "VL1-197",
-            location: { x: 5, y: 15 },
-        },
-
-        {
-            id: "VL1-101-1",
-            name: "VL1-101-1",
-            location: { x: 5, y: 11 },
-        },
-
-        {
-            id: "VL1-101-2",
-            name: "VL1-101-2",
-            location: { x: 4, y: 11 },
-        },
-
-        {
-            id: "VL1-101-7",
-            name: "VL1-101-7",
+            id: "VL-101-7",
+            name: "VL-101-7",
             location: { x: 4, y: 1 },
         },
 
         {
-            id: "VL1-101-6",
-            name: "VL1-101-6",
-            location: { x: 3, y: 1 },
-        },
-
-        {
-            id: "VL1-102",
-            name: "VL1-102",
+            id: "VL-102",
+            name: "VL-102",
             location: { x: 6, y: 1 },
         },
 
+        {
+            id: "VL-122-1",
+            name: "VL-122-1",
+            location: { x: 10, y: 4 },
+        },
 
+        {
+            id: "VL-124",
+            name: "VL-124",
+            location: { x: 11, y: 4 },
+        },
+        
+        {
+            id: "VL-128",
+            name: "VL-128",
+            location: { x: 12, y: 6 },
+        },
 
-      
+        {
+            id: "VL-127-4",
+            name: "VL-127-4",
+            location: { x: 16, y: 6 },
+        },
+
+        {
+            id: "VL-127-5",
+            name: "VL-127-5",
+            location: { x: 16, y: 7 },
+        },
+
+        {
+            id: "VL-126",
+            name: "VL-126",
+            location: { x: 12, y: 8 },
+        },
+
+        {
+            id: "VL-101-1",
+            name: "VL-101-1",
+            location: { x: 5, y: 11 },
+        },
+
+        {
+            id: "VL-101-2",
+            name: "VL-101-2",
+            location: { x: 4, y: 11 },
+        },
+
+        {
+            id: "VL-103-1",
+            name: "VL-103-1",
+            location: { x: 5, y: 13 },
+        },
+
+        {
+            id: "VL-106-1",
+            name: "VL-106-1",
+            location: { x: 10, y: 13 },
+        },
+
+        {
+            id: "VL-106-2",
+            name: "VL-106-2",
+            location: { x: 11, y: 13 },
+        },
+
+        {
+            id: "VL-116",
+            name: "VL-116",
+            location: { x: 12, y: 13 },
+        },
+
+        {
+            id: "VL-121-3",
+            name: "VL-121-3",
+            location: { x: 16, y: 13 },
+        },
+
+        {
+            id: "VL-197",
+            name: "VL-197",
+            location: { x: 5, y: 15 },
+        },
+
     ],
+
     bathrooms: [
         {
-            id: "H-812",
-            name: "bathroom-male",
-            location: { x: 15, y: 12 },
-        }
-
-    ],
-    stairs: [
-        {
-            id: "H-8-stairs-1",
-            name: "Stairs 1",
-            location: { x: 8, y: 8 },
-        },
-
-    ],
-    elevators: [
-        {
-            id: "H-8-elevator-1",
-            name: "Elevator 1",
-            location: { x: 13, y: 6 },
-        },
-    ],
-    exits: [
-        {
-            id: "H8-exit",
-            name: "H8 Exit",
-            location: { x: 8, y: 7 },
-        },
-    ]
-
-
-  },
-
-  // 2th floor
-
-  VL2: {
-    startLocation: {
-      id: "VL2-start",
-      name: "2nd Floor Entrance",
-      location: { x: 8, y: 9 },
-      type: "start"
-    },
-
-    rooms: [
-        {
-            id: "H-961.19",
-            name: "H-961.19",
-            location: { x: 1, y: 0 },
-        },
-
-    ],
-    bathrooms: [
-        {
-            id: "H-992",
+            id: "VL-118",
             name: "bathroom-neutral",
-            location: { x: 13, y: 7 },
-        },
-
-    ],
+            location: { x: 13, y: 12 },
+        },  
+     ],
     stairs: [
         {
-            id: "H-9-stairs-1",
+            id: "VL-1-stairs-1",
             name: "Stairs 1",
-            location: { x: 8, y: 8 },
+            location: { x: 1, y: 1 },
         },
-
+        {
+            id: "VL-1-stairs-2",
+            name: "Stairs 2",
+            location: { x: 7, y: 15 },
+        },
+        {
+            id: "VL-1-stairs-3",
+            name: "Stairs 3",
+            location: { x: 13, y: 10 },
+        },
     ],
     elevators: [
         {
-            id: "H-9-elevator-1",
+            id: "VL-1-elevator-1",
             name: "Elevator 1",
-            location: { x: 13, y: 6 },
-        },
+            location: { x: 9, y: 3 },
+        }, 
     ],
     exits: [
         {
-            id: "H-9-stairs-to-H8",
-            name: "Stairs to H8",
-            location: { x: 10, y: 8 },
+            id: "VL-1-exit",
+            name: "VL-1 Exit",
+            location: { x: 8, y: 17 },
         },
     ]
-
   },
+
 
   // VL-2
   2: {
