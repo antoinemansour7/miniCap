055b40738a6df44a4509f2bc1fbd92dfc923e024
Nabih21 <126896277+Nabih21@users.noreply.github.com>
2025-04-06 00:55:55 -0400

    Update directions.jsx
    debugging grid

--- FindMyClass/app/screens/directions.jsx
+++ FindMyClass/app/screens/directions.jsx
@@ -1005,14 +1005,14 @@ export default function DirectionsScreen() {
                             />)
                           }
 
-                          {/* {gridLines.map((line, index) => (
+                          {gridLines.map((line, index) => (
                               <Polyline
                                 key={index}
                                 coordinates={line}
                                 strokeWidth={1}
                                 strokeColor="rgba(0, 0, 255, 0.5)" // ✅ Light blue for debug
                               />
-                            ))} */}
+                            ))}
 
           </MapView>
           
